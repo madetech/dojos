b781dcc1a3ba04ccf453c5c60cfe49217a5cede3
Ryan MacGillivray <rmacg89@gmail.com>
2017-05-25 16:05:49 +0100

    Move README and create elixir project

---
+++ lcd/elixir_lcd/lib/elixir_lcd.ex
@@ -0,0 +1,18 @@
+defmodule ElixirLcd do
+  @moduledoc """
+  Documentation for ElixirLcd.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirLcd.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
